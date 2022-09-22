af6bc56361d4c85ec90e6f023f020e4a46c4c00e
Robert Ball <bobball89@gmail.com>
2022-09-22 00:59:45 +0000

    added clothes schema

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const { Sequelize, DataTypes } = require('sequelize');
 const foodSchema = require('./food.schema');
+const clothesSchema = require('./clothes.schema');
 
 // 'postgres://localhost:5432/api-app'
 // 'postgres://username:password@localhost:5432/api-app'
@@ -12,8 +13,7 @@ const DATABASE_URL = process.env.DATABASE_URL;
 //instantiates our database
 const sequelizeDatabase = new Sequelize(DATABASE_URL);
 
-// create People model with our Schema
-
 const FoodModel = foodSchema(sequelizeDatabase, DataTypes);
+const ClothesModel = clothesSchema(sequelizeDatabase, DataTypes);
 
-module.exports = {sequelizeDatabase, FoodModel};
+module.exports = {sequelizeDatabase, FoodModel, ClothesModel};
