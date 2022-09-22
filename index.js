30f642e4e5c59fa5a6889dcae8f4f1122c1015b1
Robert Ball <bobball89@gmail.com>
2022-09-22 00:23:21 +0000

    server up and running

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict';
 
-const { sequelizeDatabase, PeopleModel } = require('./src/models');
+const { sequelizeDatabase } = require('./src/models');
+const { start } = require('./src/server');
 
 // sync database and proof of life. Creates all associated tables
 sequelizeDatabase.sync()
-  .then(() => console.log('connection success'))
+  .then(() => {
+    console.log('Successful Connection');
+  })
   .catch(err => console.error(err));
+
+start();
